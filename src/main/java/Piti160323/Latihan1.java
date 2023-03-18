c46ec5cb0c21dda39c1e998e652db2aaaad25329
fitrisriwahyuni <127276798+fitrisriwahyuni@users.noreply.github.com>
2023-03-18 09:08:14 +0700

    Initial commit

---
+++ src/main/java/Piti160323/Latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Piti160323;
+
+/**
+ *
+ * @author USER
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 45;
+        int rata;
+        
+        rata = (a+b+C)/3;
+        System.out.println("number 1 = "+a);
+        
+    }
+    
+}
---
+++ src/main/java/com/mycompany/pbo_2211082036/PBO_2211082036.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_2211082036;
+
+/**
+ *
+ * @author USER
+ */
+public class PBO_2211082036 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
